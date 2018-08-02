b401f321cef86c39d8e2e57e0319e0195faf4e4c
denis <d.mlglego@mail.ru>
2018-08-02 15:15:54 +0300

    update hw8

--- lesson8_hw/translater.py
+++ lesson8_hw/translater.py
@@ -1,36 +1,47 @@
 import requests
 import json
 import os
+import glob
 
 API_KEY = 'trnsl.1.1.20180726T173134Z.984a03fdc7308fb6.acd681f1a3aa09163e2123dfca633d3c00e8d1f2'
-INIT_DATA_PATH = os.path.join(os.path.abspath(''), 'initial_data')
-RES_DATA_PATH = os.path.join(os.path.abspath(''), 'res_data')
+INIT_DATA_DIR = 'initial_data'
+RES_DATA_DIR = 'res_data'
+YANDEX_TRANSLATE_URL = 'https://translate.yandex.net/api/v1.5/tr.json/translate'
 
 
-def get_text_from_file(file_name):
-    with open(os.path.join(INIT_DATA_PATH, file_name), encoding='utf-8') as f:
+def get_text_from_file(file_path):
+    with open(file_path, encoding='utf-8') as f:
         return f.read()
 
 
 def create_result_file_translate(file_name, new_text):
     file_res_name = file_name[:file_name.find('.')] + '_res.txt'
 
-    with open(os.path.join(RES_DATA_PATH, file_res_name), 'w', encoding='utf-8') as file_res:
+    with open(file_res_name, 'w', encoding='utf-8') as file_res:
         file_res.write(new_text)
 
 
-def get_translated_text(text_file, lang_from, lang_to):
-    response = requests.get(('https://translate.yandex.net/api/v1.5/tr.json/'
-                             'translate?key={0}&text={1}&lang={2}-{3}'.format(API_KEY, text_file, lang_from, lang_to)))
-    return json.loads(response.text)['text'][0].strip()
+def get_translated_text(text_file, lang_to):
+    try:
+        params = {'key': API_KEY, 'text': text_file, 'lang': lang_to}
+        response = requests.get(YANDEX_TRANSLATE_URL, params=params)
+
+        return json.loads(response.text)['text'][0].strip()
+    except:
+        return 'TRNSLTE ERROR'
 
 
-def translate_text(file_name, lang_from, lang_to='ru'):
-    text_file = get_text_from_file(file_name)
-    text_translated = get_translated_text(text_file, lang_from, lang_to)
-    create_result_file_translate(file_name, text_translated)
+def translate_text(file_path, lang_to='ru'):
+    text_file = get_text_from_file(file_path)
+    text_translated = get_translated_text(text_file, lang_to)
+
+    file_path = file_path.replace(INIT_DATA_DIR, RES_DATA_DIR)
+    create_result_file_translate(file_path, text_translated)
 
 
-translate_text('DE.txt', 'de', 'ru')
-translate_text('ES.txt', 'es', 'ru')
-translate_text('FR.txt', 'fr', 'ru')
+# translate_text('DE.txt', 'de', 'ru')
+# translate_text('ES.txt', 'es', 'ru')
+# translate_text('FR.txt', 'fr', 'ru')
+
+for file in glob.glob(f'{INIT_DATA_DIR}/*.txt'):
+    translate_text(file)
