14f51ac87cce670a6cb103efba71d72b88b29c86
Denis <d.mlglego@mail.ru>
2018-08-11 22:32:15 +0300

    refractor first version

--- Diploma/diploma.py
+++ Diploma/diploma.py
@@ -15,36 +15,64 @@ def send_req(method, params):
     return requests.get(f'https://api.vk.com/method/{method}', params=params)
 
 
-def main():
-    investigated = '277079908'
-    def_params = {
-        'access_token': TOKEN_VK_API,
-        'v': 5.80,
-        'user_id': investigated,
-        'extended': True,
-    }
-    group_params = {**def_params, 'fields': 'members_count'}
+def process_param(user_id, params, extended, add_params):
+    add_params = {} if add_params is None else add_params
+    if params is None:
+        params = {
+            'access_token': TOKEN_VK_API,
+            'v': 5.80,
+            'user_id': user_id,
+            'extended': extended
+        }
+    try:
+        int(user_id)
+        params['user_id'] = user_id
+    except ValueError:
+        params['screen_name'] = user_id
 
-    investigated_friends = set(send_req(GET_FRIENDS, params=def_params).json()['response']['items'])
-    investigated_groups = send_req(GET_GROUPS, params=group_params).json()['response']['items']
+    params.update(add_params)
+    return params
+
+
+def get_groups(user_id, params=None, extended=0, add_params=None):
+    params = process_param(user_id, params, extended, add_params)
+    return send_req(GET_GROUPS, params=params).json()['response']['items']
+
+
+def get_friends(user_id, params=None, extended=0, add_params=None):
+    params = process_param(user_id, params, extended, add_params)
+    return send_req(GET_FRIENDS, params=params).json()['response']['items']
+
+
+def main():
+    investigated = input('Введите ник или id для обработки: ')
+    permissible_friend = int(input('Введите допустимое количество друзей в группах'))
+
+    investigated_friends = set(get_friends(investigated))
+    investigated_groups = get_groups(investigated, extended=True, add_params={'fields': 'members_count'})
 
     investigated_groups_id = {group['id'] for group in investigated_groups}
 
     all_groups_friends = set()
 
+    count_friend = len(investigated_friends)
+    
     for friend in investigated_friends:
-        print('Отправляем запрос..')
-        try:
-            group_friend = send_req(GET_GROUPS, params={
-                'access_token': TOKEN_VK_API,
-                'v': 5.80,
-                'user_id': friend
-            }).json()['response']['items']
+        print(f'Осталось обработать {count_friend} друзей')
+        print('Отправляем запрос...')
 
+        try:
+            group_friend = get_groups(friend)
             all_groups_friends.update(group_friend)
+
         except (KeyError, requests.exceptions.RequestException):
             pass
-        time.sleep(1)
+
+        finally:
+            count_friend -= 1
+            time.sleep(0.5)
+
+    print('Подготавливаем результат...')
 
     succcess_groups_id = investigated_groups_id - all_groups_friends
     succes_groups = []
@@ -57,7 +85,10 @@ def main():
                 'members_count': group['members_count']
             })
 
-    res = json.dumps(succes_groups, ensure_ascii=False)
+    with open('groups.json', 'w+', encoding='utf8') as res_file:
+        json.dump(succes_groups, res_file, ensure_ascii=False, indent=2)
+
+    print('Результат работы в файле groups.json')
 
 
 main()
